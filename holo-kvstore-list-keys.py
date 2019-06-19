5f7bc1dab7e70985bfc53609e7ba3bcd493dbe33
Paul B. Hartzog <paul.hartzog@holo.host>
2019-06-19 13:05:22 -0400

    created 'read' svcs'

---
+++ holo-kvstore-list-keys.py
@@ -0,0 +1,84 @@
+from zato.server.service import Service
+from json import dumps
+import httplib
+
+class HoloKVStoreListKeys(Service):
+    def checkKey(self, d, key):
+        return key in d
+
+    def kvdb_get( self, system, key ):
+        # kvdb
+        service = 'zato.kvdb.data-dict.dictionary.get-value-list'
+        request = { "system":system, "key":key }
+        response = self.invoke( service, request )
+        return response["zato_kvdb_data_dict_dictionary_get_value_list_response"][0]["name"]
+
+
+    def handle(self):
+        # miscellaneous logging
+        self.logger.info(self.request.raw_request)
+        self.logger.info('cid:[{}]'.format(self.cid))
+        # prep data
+        data = self.request.bunchified()
+        self.logger.info(data)
+
+        # exit if missing incoming data
+        if not self.checkKey(data, 'kv_store'):
+            response = {"error": "request must contain a kv store namespace identifier"}
+            self.response.status_code = httplib.BAD_REQUEST
+            self.response.payload = dumps(response)
+            return
+        if not data.kv_store:
+            response = {"error": "kv_store value must not be empty"}
+            self.response.status_code = httplib.BAD_REQUEST
+            self.response.payload = dumps(response)
+            return
+
+        auth_email = self.kvdb_get( 'cloudflare', 'auth_email' )
+        self.logger.info(auth_email)
+        auth_key = self.kvdb_get( 'cloudflare', 'auth_key' )
+        self.logger.info(auth_key)
+        # Headers the endpoint expects
+        headers = {'X-Auth-Email':auth_email, 'X-Auth-Key':auth_key, 'Content-Type':'application/json'}
+
+        account = self.kvdb_get( 'cloudflare', 'account' )
+        self.logger.info(account)
+
+        # get the id for the kv_store
+        namespace = data.kv_store
+        if namespace == "dns2hash":
+            namespace = self.kvdb_get( 'cloudflare', 'kvstore_dns2hash' )
+        if namespace == "hash2tranche":
+            namespace = self.kvdb_get( 'cloudflare', 'kvstore_hash2tranche' )
+        self.logger.info(namespace)
+
+        # params
+        params = {"account":account, "namespace":namespace}
+
+        # Obtains a connection object
+        conn = self.outgoing.plain_http['cloudflare-kvstore-list-keys'].conn
+
+        # Invoke the resource providing all the information on input
+        response = conn.get(self.cid, params, headers=headers)
+        # if no response
+        """
+        if not response:
+            response = {"error": "no response"}
+            self.response.status_code = httplib.BAD_GATEWAY # 502
+            self.response.payload = dumps(response)
+            return
+            """
+        # else
+        self.response.payload = dumps(response.json())
+        return
+
+"""
+
+uses the following server objects
+
+outconn:cloudflare-kvstore-list-keys:
+    https://api.cloudflare.com
+    /client/v4/accounts/{account}/storage/kv/namespaces/{namespace}/keys
+    Cloudflare API
+
+"""
