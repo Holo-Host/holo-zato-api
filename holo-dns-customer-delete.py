b682bbb1b80cd596584ad71d3eb5c152ac3f027c
Paul B. Hartzog <paul.hartzog@holo.host>
2019-07-16 13:45:08 -0400

    add holo-dns-customer-delete

---
+++ holo-dns-customer-delete.py
@@ -0,0 +1,105 @@
+from zato.server.service import Service
+from json import dumps
+import httplib
+
+class HoloDnsCustomerDelete(Service):
+    def checkKey(self, d, key):
+        return key in d
+
+    def handle(self):
+        # miscellaneous logging
+        self.logger.info(type(self.request.raw_request))
+        self.logger.info(self.request.raw_request)
+        self.logger.info('cid:[{}]'.format(self.cid))
+        # prep data
+        data = self.request.bunchified()
+        # exit if no domain incoming
+        if not self.checkKey(data, 'domain'):
+            response = {"error": "request must contain a domain"}
+            self.response.status_code = httplib.BAD_REQUEST
+            self.response.payload = dumps(response)
+            return
+        # else
+
+        # kvdb
+        service = 'zato.kvdb.data-dict.dictionary.get-value-list'
+
+        ddrequest = {'system':'cloudflare', 'key': 'auth_key'}
+        ddresponse = self.invoke(service, ddrequest)
+        auth_key = ddresponse["zato_kvdb_data_dict_dictionary_get_value_list_response"][0]["name"]
+        self.logger.info(auth_key)
+
+        ddrequest = {'system':'cloudflare', 'key': 'auth_email'}
+        ddresponse = self.invoke(service, ddrequest)
+        auth_email = ddresponse["zato_kvdb_data_dict_dictionary_get_value_list_response"][0]["name"]
+        self.logger.info(auth_email)
+
+        # Headers the endpoint expects
+        headers = {'X-Auth-Email':auth_email, 'X-Auth-Key':auth_key}
+
+        ddrequest = {'system':'cloudflare', 'key': 'zone'}
+        ddresponse = self.invoke(service, ddrequest)
+        zone = ddresponse["zato_kvdb_data_dict_dictionary_get_value_list_response"][0]["name"]
+        self.logger.info(zone)
+
+        # ---------------- Part One -------------------- #
+        # get domain id for domain
+
+        # params
+        domain = data.domain + ".holohost.net"
+        dtype = "CNAME"
+        params = {"zone":zone, "type":dtype, "domain":domain}
+
+        # Obtains a connection object
+        conn = self.outgoing.plain_http['cloudflare-dns-by-name'].conn
+
+        # Invoke the resource providing all the information on input
+        try:
+            response = conn.get(self.cid, params, headers=headers)
+            response_dict = response.json()
+            domain_id = response_dict["result"][0]["id"]
+        except:
+            response = {"error": "something broke"}
+            self.response.status_code = httplib.BAD_GATEWAY # 502
+            self.response.payload = dumps(response)
+            return
+        else:
+            # ---------------- Part Two -------------------- #
+            # DELETE domain by id
+
+            # params
+            params = {"zone":zone, "record_id":domain_id}
+
+            # Obtains a connection object
+            conn = self.outgoing.plain_http['cloudflare-dns-delete'].conn
+
+            # Invoke the resource providing all the information on input
+            response = conn.delete(self.cid, params, headers=headers)
+
+            # if no response
+            """
+            if not response:
+                response = {"error": "no response"}
+                self.response.status_code = httplib.BAD_GATEWAY # 502
+                self.response.payload = dumps(response)
+                return
+                """
+            # else
+            self.response.payload = dumps(response.json())
+            return
+
+"""
+
+uses the following server objects
+
+outconn:cloudflare-dns-read:
+    https://api.cloudflare.com
+    /client/v4/zones/{zone}/dns_records
+    Cloudflare API
+
+outconn:cloudflare-dns-delete:
+    https://api.cloudflare.com
+    /client/v4/zones/{zone}/dns_records/{record_id}
+    Cloudflare API
+
+"""
